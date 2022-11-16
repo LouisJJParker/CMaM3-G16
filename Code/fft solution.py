cf5ee5477889767889e4481d575d378c2344b4ff
Koulao <115569119+HaziqFah26@users.noreply.github.com>
2022-11-16 12:08:36 +0000

    Update fft solution.py

--- Code/fft solution.py
+++ Code/fft solution.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 
 # f = data
 # N = number of oscillations
+# dt = time step
 
 y = fftpack.rfft(f)
 yf = np.abs(y)
@@ -26,5 +27,18 @@ p3_val = np.take(yf_amp_3, p3)
 p4, _ = find_peaks(yf_amp_4)
 p4_val = np.take(yf_amp_4, p4)
 
-plt.plot(p1, p1_val, 'r-', p2, p2_val, 'b', p3, p3_val, 'g', p4, p4_val, 'y')
+p1 = p1*dt
+p2 = p2*dt
+p3 = p3*dt
+p4 = p4*dt
+
+plt.xlabel('Time')
+plt.ylabel('Energy')
+plt.title('FFT')
+
+plt.plot(p1, p1_val, 'r-', label='First Coefficient')
+plt.plot(p2, p2_val, 'b-', label='Second Coefficient')
+plt.plot(p3, p3_val, 'g-', label='Third Coefficient')
+plt.plot(p4, p4_val, 'y-', label='Fourth Coefficient')
+plt.legend()
 plt.show()
